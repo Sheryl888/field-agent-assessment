7a86462aabcdfdede3d545ded05f680e0b772f5e
Sheryl888 <sherylmlange@gmail.com>
2022-04-03 22:25:53 -0500

    status after review

--- src/main/java/learn/field_agent/App.java
+++ src/main/java/learn/field_agent/App.java
@@ -2,8 +2,10 @@ package learn.field_agent;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan  //do I also need this here? I only had the @SpringBootApplication
 public class App {
 
     public static void main(String[] args) {
