029b65aa495514e094d4bfff563c2d1f4b271926
Sheryl888 <sherylmlange@gmail.com>
2022-04-08 14:27:07 -0500

    set up back-end

--- src/main/java/learn/field_agent/App.java
+++ src/main/java/learn/field_agent/App.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
-@ComponentScan  //do I also need this here? I only had the @SpringBootApplication
+
 public class App {
 
     public static void main(String[] args) {
--- src/main/java/learn/field_agent/models/Agent.java
+++ src/main/java/learn/field_agent/models/Agent.java
@@ -82,6 +82,4 @@ public class Agent {
         this.alias = alias;
     }
 
-
-
 }
